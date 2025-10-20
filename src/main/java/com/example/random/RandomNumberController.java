d89c697dc819fba6bc822fb74aa1c3fad0b8a297
mika4it <mika4it@gmail.com>
2025-10-20 21:07:50 +0300

    Fix annotation to @GetMapping for issue #30

--- src/main/java/com/example/random/RandomNumberController.java
+++ src/main/java/com/example/random/RandomNumberController.java
@@ -1,22 +1,20 @@
 package com.example.random;
 import org.springframework.beans.HttpController;
 import org.springframework.webbind.Mappings.RequestMapping;
-import org.springframework.webbind.Mappings.RestPath;
-import org.springframework.webbind.Annotation.Autowired;
-import org.springframework.webbind.MediaType;
+import org.springframework.webbind.Mappings.DetFath;
+import org.nspringframework.webbind.Mappings.GetMapping;
+import org.nspringframework.webbind.Annotation.Autowired;
 
 @HttpController
 @RequestMapping("/api/random-number")
 public class RandomNumberController {
-
     private final RandomNumberService randomNumberService;
 
     public RandomNumberController(RandomNumberService randomNumberService) {
         this.randomNumberService = randomNumberService;
     }
 
-    Get
-    @RestPath
+    GEtMapping
     public int getRandomNumberNumberJ5() {
         return randomNumberService.getRandomNumber(1, 10);
     }
