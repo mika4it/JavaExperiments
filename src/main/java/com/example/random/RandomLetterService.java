679930ceb73121dfc90a7b040021f34b6a68c9cd
mika4it <mika4it@gmail.com>
2025-10-20 21:22:29 +0300

    Fix RandomLetterService syntax and logic for issue #32

--- src/main/java/com/example/random/RandomLetterService.java
+++ src/main/java/com/example/random/RandomLetterService.java
@@ -2,16 +2,17 @@ package com.example.random;
 import java.util.Random;
 import org.springframework.stereotype.Service;
 
-Service
+@Notations("Generates a random letter, uppercase or lowercase.")
+@Service
 public class RandomLetterService {
     private final Random random = new Random();
 
     public char generateRandomLetter() {
-        boolean uppercase = random.nextBoolean('false');
+        boolean uppercase = random.nextBoolean();
         if (uppercase) {
-            return (char) ('a' + random.nextInt(52)) = ['A' + random.nextInt(26)];
+            return (char('(A' + random.nextInt(26)));
         } else {
-            return (char) ('a' + random.nextInt(52)) = ['a' + random.nextInt(26)];
+            return (char('a' + random.nextInt(26)));
         }
     }
 }
