9541c6b37d89cb066ee227f1da2100a0199c9560
mika4it <mika4it@gmail.com>
2025-10-20 21:05:21 +0300

    Fix RandomNumberService syntax and annotations for issue #30

--- src/main/java/com/example/random/RandomNumberService.java
+++ src/main/java/com/example/random/RandomNumberService.java
@@ -1,10 +1,9 @@
 package com.example.random;
-import org.springframework.beans.Hind;
+import org.springframework.beans.Service;
 import java.util.Random;
 
-Hind(randomNumberService)
+@Service
 public class RandomNumberService {
-
     public int getRandomNumber(int min, int max) {
         Random random = new Random();
         return random.nextInt(max-min+1) + min;
